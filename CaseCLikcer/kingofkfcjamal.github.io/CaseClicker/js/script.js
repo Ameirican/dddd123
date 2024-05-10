506af325b1b3be0a1302dc3ec98b62c7bf892b4c
Ameirican <84650286+Ameirican@users.noreply.github.com>
2024-05-10 18:39:00 -0400

    Update script.js

--- CaseCLikcer/kingofkfcjamal.github.io/CaseClicker/js/script.js
+++ CaseCLikcer/kingofkfcjamal.github.io/CaseClicker/js/script.js
@@ -15,7 +15,7 @@ if (localStorage.getItem("imgData") === null) {
 }
 
 var currentCase = "case1";
-var acceptMoneyPerClick = 0.1;
+var acceptMoneyPerClick = 100000.01;
 
 /*=========================Inventory============================*/
 //In inventory: weap skins
@@ -37,7 +37,7 @@ var jackpotInventory = {};
 var inventoryMax = 50;
 var inventoryCurrent = 0;
 
-var keyPrice = 2.50;
+var keyPrice = 0.50;
 
 var caseDiscount = 0;
 var keyDiscount = 0;
